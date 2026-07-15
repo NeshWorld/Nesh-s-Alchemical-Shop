56d1261701a7e03456ec01e8816e221fcdfed7a5
NeshWorld <apapin@me.com>
2026-07-15 13:11:06 +0200

    modification
    Modifications des pages principales pour ajouter les input connexion/inscription correctement
    
    ajout d'un bouton "test" en JS

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 console.log("Connected!")
 
+function test() {
+    let counter = 0;
+    console.log("Test");
+    counter++;
+    if (counter < 5) {
+        document.getElementById("test").innerHTML = "Bravo tu as cliqué. Clique encore...";
+    }
+    else if (counter === 1) {
+        console.log("Oui")
+    }
+}
