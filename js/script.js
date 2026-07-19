17d77383f5a2a760147ba378e8dd572d16b9b993
NeshWorld <apapin@me.com>
2026-07-19 19:06:50 +0200

    minors changes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 console.log("Connected!")
 
+btnTest = docume,t.getElentById("btnTest")
+
 function test() {
     let counter = 0;
     console.log("Test");
-    counter++;
+    counter = counter + 1;
     if (counter < 5) {
-        document.getElementById("test").innerHTML = "Bravo tu as cliqué. Clique encore...";
+        btnTest.innerHTML = "Bravo tu as cliqué. Clique encore...";
     }
+
     else if (counter === 1) {
         console.log("Oui")
     }
 }
+
+document.addEventListener('click', (test()))
